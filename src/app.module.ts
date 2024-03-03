6c078a125188be438c185b48a0ec052d36b5fe16
Lucas <lucas@MacBook-Air-de-Lucas.local>
2024-03-03 15:47:42 -0300

    corrigindo bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://lucasmourap:saak39fd45A54Hvr@cluster0.2ziwkhz.mongodb.net/test',
+      'mongodb+srv://lucasmourap:saak39fd45A54Hvr@cluster0.2ziwkhz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     ),
     UsersModule,
   ],
